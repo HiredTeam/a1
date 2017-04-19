baa550b516c4c72f0eae0c77fa0e99db36f2c563
isb <s.bezruchenko@gmail.com>
2017-04-19 19:34:13 +0300

    Сообщение от коммита

--- okno.py
+++ okno.py
@@ -1,6 +1,11 @@
-file = open("c:\temp\poem.txt", 'w')
-
+import random
+file = open("c:\Temp\Snumbers.txt", 'w+')
 poem = "I never saw a man who looked\n"
+i=0
 
-file.write(poem)
+while i<=1000:
+    numbs = random.sample(range(1, 1000), 10)
+    file.write(str(numbs)+"\n")
+    i+=1
+
 file.close()
