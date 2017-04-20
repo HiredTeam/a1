5f551df3a39219b9fd3017be9ff6a808ae31ce42
isb <s.bezruchenko@gmail.com>
2017-04-20 23:22:24 +0300

    Сообщение от коммита

--- okno.py
+++ okno.py
@@ -1,15 +1,17 @@
 import random
 from datetime import *
 
-file = open("c:\Temp\Snumbers.txt", 'w+')
+file = open(r"c:\Temp\numbers.txt", "w+")
 poem = "I never saw a man who looked\n"
 i = 1
+<<<<<<< HEAD
 #   yherty
 while i <= 100:
+>>>>>>> 2d5488d7e4fa2b681e2931626babee801e2f76b9
     numbs = random.sample(range(1, 100), 10)
     sum_stroki = 0
     for item in numbs:
         sum_stroki = sum_stroki + item
-    file.write(str(datetime.today()) + '  ' + str(i) + '  ' + str(sum_stroki) + '  ' + str(numbs) + "\n")
+    file.write(str(datetime.today()) + '\t' + str(i) + '\t' + str(sum_stroki) + ' \t' + str(numbs) + "\n")
     i += 1
 file.close()
